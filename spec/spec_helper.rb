a704f7bee239de86cc9bdb53d7dbe0623f11e290
Naumenko Nikita <naumenkoniki@gmail.com>
2018-08-22 21:43:07 +0300

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,20 +2,18 @@ require 'rack/test'
 require 'rspec'
 require 'pry'
 
-Dir[File.expand_path('lib/**/*.rb')].each { |file| require_relative file }
+Dir[File.expand_path('lib/**/*.rb')].each { |file| require file }
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 
-    # conf.expect_with :rspec do |expectations|
-    # expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  # conf.expect_with :rspec do |expectations|
+  # expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   # end
 
   # conf.mock_with :rspec do |mocks|
-    # mocks.verify_partial_doubles = true
+  # mocks.verify_partial_doubles = true
   # end
 
   # conf.shared_context_metadata_behavior = :apply_to_host_groups
-
-
 end
